480b96741cfe72ce358f0bde53997ad1372b50aa
jinbeom, jeong <ppgt1225@naver.com>
2023-11-11 00:07:07 +0900

    fix data load

--- utils/dataset.py
+++ utils/dataset.py
@@ -2,97 +2,46 @@ import os
 import numpy as np
 import pandas as pd
 
+from tqdm import tqdm
 
-def load_dataset(train_dataset_indices: np.ndarray, val_dataset_indices: np.ndarray) -> (np.ndarray, np.ndarray):
-    data_root_path = 'data'+os.sep
-    data_file_names = os.listdir('data')
 
-    pipe_length_arr = np.array([], dtype=np.float64)
-    venturi_pump_height_arr = np.array([], dtype=np.float64)
-    motor_speed_arr = np.array([], dtype=np.float64)
+pipe_length_30m_indices = np.array([18, 17, 22, 21, 5, 1, 6, 2, 26, 29, 25, 30, 13, 9, 14, 10], dtype=np.uint8)
+pipe_length_45m_indices = np.array([24, 23, 20, 19, 3, 4, 7, 8, 27, 32, 28, 31, 16, 15, 12, 11], dtype=np.uint8)
+venturi_pump_65cm_indices = np.array([18, 20, 19, 17, 3, 4, 1, 2, 26, 25, 27, 28, 9, 10, 12, 11], dtype=np.uint8)
+venturi_pump_45cm_indices = np.array([24, 23, 22, 21, 5, 6, 7, 8, 29, 32, 30, 31, 16, 13, 15, 14], dtype=np.uint8)
+motor_speed_750_indices = np.array([23, 19, 17, 21, 3, 5, 1, 7, 29, 25, 27, 31, 13, 15, 9, 11], dtype=np.uint8)
+motor_speed_900_indices = np.array([24, 18, 20, 22, 4, 6, 8, 2, 26, 32, 28, 30, 16, 14, 10, 12], dtype=np.uint8)
 
-    main_pump_inlet_pressure_train_arr = np.array([], dtype=np.float64)
-    main_pump_outlet_pressure_train_arr = np.array([], dtype=np.float64)
-    venturi_pump_inlet_pressure_train_arr = np.array([], dtype=np.float64)
-    main_pump_outlet_flowrate_train_arr = np.array([], dtype=np.float64)
-    venturi_pump_outlet_flowrate_train_arr = np.array([], dtype=np.float64)
+data_root_path = 'data' + os.sep
+data_file_names = os.listdir('data')
 
-    main_pump_inlet_pressure_val_arr = np.array([], dtype=np.float64)
-    main_pump_outlet_pressure_val_arr = np.array([], dtype=np.float64)
-    venturi_pump_inlet_pressure_val_arr = np.array([], dtype=np.float64)
-    main_pump_outlet_flowrate_val_arr = np.array([], dtype=np.float64)
-    venturi_pump_outlet_flowrate_val_arr = np.array([], dtype=np.float64)
 
-    pipe_length_30m_indices = np.array([18, 17, 22, 21, 5, 1, 6, 2, 26, 29, 25, 30, 13, 9, 14, 10], dtype=np.uint8)
-    pipe_length_45m_indices = np.array([24, 23, 20, 19, 3, 4, 7, 8, 27, 32, 28, 31, 16, 15, 12, 11], dtype=np.uint8)
-    venturi_pump_height_30cm_indices = np.array([18, 20, 19, 17, 3, 4, 1, 2, 26, 25, 27, 28, 9, 10, 12, 11], dtype=np.uint8)
-    venturi_pump_height_50cm_indices = np.array([24, 23, 22, 21, 5, 6, 7, 8, 29, 32, 30, 31, 16, 13, 15, 14], dtype=np.uint8)
-    motor_speed_25hz_indices = np.array([23, 19, 17, 21, 3, 5, 1, 7, 29, 25, 27, 31, 13, 15, 9, 11], dtype=np.uint8)
-    motor_speed_30hz_indices = np.array([24, 18, 20, 22, 4, 6, 8, 2, 26, 32, 28, 30, 16, 14, 10, 12], dtype=np.uint8)
+def load_dataset(train_dataset_indices: np.ndarray, val_dataset_indices: np.ndarray):
+    train_dataset = pd.DataFrame()
+    val_dataset = pd.DataFrame()
+    motor_speed = 0
 
-    pipe_length = 0  # unit: meter
-    venturi_pump_height = 0  # unit: cent-meter
-    motor_speed = 0  # unit: hz
-
-    for i, data_file_name in enumerate(data_file_names):  # load csv data and extract data
+    for data_file_name in tqdm(data_file_names, desc='load dataset', ncols=80):
+        raw_data = pd.read_csv(data_root_path + data_file_name)
         run_index = int(data_file_name[8:10])
 
-        if np.any(run_index == motor_speed_25hz_indices):
-            motor_speed = 25
+        if np.any(run_index == motor_speed_750_indices):
+            motor_speed = 750
 
-        if np.any(run_index == motor_speed_30hz_indices):
-            motor_speed = 30
+        if np.any(run_index == motor_speed_900_indices):
+            motor_speed = 900
 
-        if motor_speed == 25:
-            raw_data = pd.read_csv(data_root_path+data_file_name)
+        motor_speed_arr = np.full(shape=raw_data.shape[0], fill_value=motor_speed)
 
-            if np.any(run_index == pipe_length_30m_indices):
-                pipe_length = 30
+        if np.any(run_index == train_dataset_indices):
+            # raw_data = pd.concat([raw_data, pd.DataFrame(motor_speed_arr, columns=['motor_speed(rpm)'])], axis=1)
+            train_dataset = pd.concat([train_dataset, raw_data], axis=0)
 
-            if np.any(run_index == pipe_length_45m_indices):
-                pipe_length = 45
+        if np.any(run_index == val_dataset_indices):
+            # raw_data = pd.concat([raw_data, pd.DataFrame(motor_speed_arr, columns=['motor_speed(rpm)'])], axis=1)
+            val_dataset = pd.concat([val_dataset, raw_data], axis=0)
 
-            if np.any(run_index == venturi_pump_height_30cm_indices):
-                venturi_pump_height = 30
+    train_dataset.drop(columns=['time(sec)', 'main_pump_outlet_F(LPM)'], inplace=True)
+    val_dataset.drop(columns=['time(sec)', 'main_pump_outlet_F(LPM)'], inplace=True)
 
-            if np.any(run_index == venturi_pump_height_50cm_indices):
-                venturi_pump_height = 50
-
-            pipe_length_arr = np.append(pipe_length_arr, np.full(shape=len(raw_data['main_pump_inlet_P(bar)'].values),
-                                                                 fill_value=pipe_length))
-            venturi_pump_height_arr = np.append(venturi_pump_height_arr, np.full(shape=len(raw_data['main_pump_inlet_P(bar)'].values),
-                                                                                 fill_value=venturi_pump_height))
-            motor_speed_arr = np.append(motor_speed_arr, np.full(shape=len(raw_data['main_pump_inlet_P(bar)'].values),
-                                                                 fill_value=motor_speed))
-
-            if np.any(run_index == train_dataset_indices):
-                main_pump_inlet_pressure_train_arr = np.append(main_pump_inlet_pressure_train_arr,
-                                                               raw_data['main_pump_inlet_P(bar)'].values)
-                main_pump_outlet_pressure_train_arr = np.append(main_pump_outlet_pressure_train_arr,
-                                                                raw_data['main_pump_outlet_P(bar)'].values)
-                venturi_pump_inlet_pressure_train_arr = np.append(venturi_pump_inlet_pressure_train_arr,
-                                                                  raw_data['venturi_pump_inlet_P(bar)'].values)
-                main_pump_outlet_flowrate_train_arr = np.append(main_pump_outlet_flowrate_train_arr,
-                                                                raw_data['main_pump_outlet_F(LPM)'].values)
-                venturi_pump_outlet_flowrate_train_arr = np.append(venturi_pump_outlet_flowrate_train_arr,
-                                                                   raw_data['venturi_pump_outlet_F(LPM)'].values)
-
-            if np.any(run_index == val_dataset_indices):
-                main_pump_inlet_pressure_val_arr = np.append(main_pump_inlet_pressure_val_arr,
-                                                             raw_data['main_pump_inlet_P(bar)'].values)
-                main_pump_outlet_pressure_val_arr = np.append(main_pump_outlet_pressure_val_arr,
-                                                              raw_data['main_pump_outlet_P(bar)'].values)
-                venturi_pump_inlet_pressure_val_arr = np.append(venturi_pump_inlet_pressure_val_arr,
-                                                                raw_data['venturi_pump_inlet_P(bar)'].values)
-                main_pump_outlet_flowrate_val_arr = np.append(main_pump_outlet_flowrate_val_arr,
-                                                              raw_data['main_pump_outlet_F(LPM)'].values)
-                venturi_pump_outlet_flowrate_val_arr = np.append(venturi_pump_outlet_flowrate_val_arr,
-                                                                 raw_data['venturi_pump_outlet_F(LPM)'].values)
-
-    train_feature_dataset = np.vstack([main_pump_inlet_pressure_train_arr, main_pump_outlet_pressure_train_arr,
-                                       venturi_pump_inlet_pressure_train_arr, main_pump_outlet_flowrate_train_arr]).T
-    val_feature_dataset = np.vstack([main_pump_inlet_pressure_val_arr, main_pump_outlet_pressure_val_arr,
-                                     venturi_pump_inlet_pressure_val_arr, main_pump_outlet_flowrate_val_arr]).T
-
-    return (train_feature_dataset, venturi_pump_outlet_flowrate_train_arr,
-            val_feature_dataset, venturi_pump_outlet_flowrate_val_arr)
+    return train_dataset, val_dataset
